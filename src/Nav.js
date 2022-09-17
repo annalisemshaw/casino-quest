b19dd02f406a98728d3d405015965c29646141b1
annalisemshaw <sha19023@byui.edu>
2022-09-16 21:47:06 -0700

    navbar edit

--- src/Nav.js
+++ src/Nav.js
@@ -3,13 +3,29 @@ import logo from './assets/CQ-logo.png'
 
 export default function Navbar() {
     return (
-        <nav className="navbar">
-            <a><img src={logo} className="navbar--logo"/></a>
-            <li><a href="./App" className="navbar--item">Home</a></li>
-            <li><a href="#" className="navbar--item">Reservations</a></li>
-            <li><a href="#" className="navbar--item">Groups {`&`} Concierge</a></li>
-            <li><a href="https://shopcasinoquest.com/" className="navbar--item">Shop</a></li>
-            <li><a href="#" className="navbar--item">More +</a></li>
+        <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <img src={logo} className="navbar-logo" />
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon  navbar-dark"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav nav">
+                <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Groups & Concierge</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="https://shopcasinoquest.com/">Shop</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link">More +</a>
+                </li>
+            </ul>
+            </div>
+        </div>
         </nav>
     )
 }
